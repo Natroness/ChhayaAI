977f17da0bbd97eabeffe2c7a57cd13fb3b866fc
Anish KC <89182274+mhosigiri@users.noreply.github.com>
2026-03-27 22:54:07 -0500

    Initial Commit

---
+++ ChhayaAI/ChhayaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChhayaAIApp.swift
+//  ChhayaAI
+//
+//  Created by Anish K C on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ChhayaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChhayaAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChhayaAI
+//
+//  Created by Anish K C on 3/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
